5445a556d58c39c5c44d2ffbe766ad9418e99c26
Raj Rao <neeraj2608@gmail.com>
2014-06-09 22:11:27 -0400

    Adding feature selection and stratified shuffle split

--- src/sk-svm/svmAuthorRec.py
+++ src/sk-svm/svmAuthorRec.py
@@ -1,4 +1,4 @@
-# Hybrid Classification for Authorship Attribution
+# Hybrid Classification on Shallow Text Analysis for Authorship Attribution
 
 import re
 from nltk import FreqDist
@@ -8,17 +8,21 @@ from os.path import join
 from os import walk
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from sklearn.cross_validation import cross_val_score, KFold, train_test_split
+from sklearn.cross_validation import cross_val_score, KFold, train_test_split, StratifiedShuffleSplit
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model.stochastic_gradient import SGDClassifier
+from sklearn.feature_selection import SelectPercentile, chi2, f_classif
 from scipy.stats import sem # standard error of mean
 import numpy as np
 import matplotlib.pyplot as pt
 
-NUMFOLDS = 10
+NUMFOLDS = 5
 RANGE = 10
 
 class MyFreqDist(FreqDist):
+    '''
+    Extend FreqDist to implement dis legomena
+    '''
     def dises(self):
         """
         @return: A list of all samples that occur twice (dis legomena)
@@ -54,6 +58,22 @@ def getFiles(dir):
     return fileList
 
 def loadFeaturesForBook(filename, smartStopWords, pronSet, conjSet):
+    '''
+    Load features for each book in the corpus. There are 3 + RANGE*4 features
+    for each instance. These features are:
+        1. number of hapax legomena divided by number of unique words
+        2. number of dis legomena divided by number of unique words
+        3. number of unique words divided by number of total words
+
+        4. no. of sentences of length in the range [1,RANGE] divided by the
+           number of total sentences
+        5. no. of words of length in the range [4,18] divided by the
+           number of total words
+        6. no. of nominative pronouns per sentence in the range [1,RANGE] divided by the
+           number of total sentences
+        7. no. of (coordinating + subordinating) conjunctions per sentence in the range
+           [1,RANGE] divided by the number of total sentences
+    '''
     text = open(filename,'r').read()
 
     contents = extractBookContents(text)
@@ -95,19 +115,19 @@ def loadFeaturesForBook(filename, smartStopWords, pronSet, conjSet):
 
     #sentenceDist = FreqDist(sentences)
     #print sentenceDist.keys()[:15] # most common sentences
-    wordsDist = MyFreqDist(FreqDist(cleanWords))
+    wordsDist = MyFreqDist(FreqDist(allWords))
     #print wordsDist.keys()[:15] # most common words
     #print wordsDist.keys()[-15:] # most UNcommon words
 
     numUniqueWords = len(wordsDist.keys())
-    numTotalWords = len(cleanWords)
+    numTotalWords = len(allWords)
 
     hapax = float(len(wordsDist.hapaxes()))/numUniqueWords # no. words occurring once / total num. UNIQUE words
     dis = float(len(wordsDist.dises()))/numUniqueWords # no. words occurring twice / total num. UNIQUE words
     richness = float(numUniqueWords)/numTotalWords # no. unique words / total num. words
 
     sentenceLengthDist = map(lambda x: sentenceLengthDistribution.freq(x), range(1,RANGE))
-    wordLengthDist = map(lambda x: wordLengthDistribution.freq(x), range(1,RANGE))
+    wordLengthDist = map(lambda x: wordLengthDistribution.freq(x), range(4,18))
     pronounDist = map(lambda x: pronounDistribution.freq(x), range(1,RANGE))
     conjunctionDist = map(lambda x: conjunctionDistribution.freq(x), range(1,RANGE))
 
@@ -120,7 +140,6 @@ def loadFeaturesForBook(filename, smartStopWords, pronSet, conjSet):
     #print pronounDist
     #print conjunctionDist
 
-    # We have 103 features in all
     result = []
     result.append(hapax)
     result.append(dis)
@@ -132,31 +151,40 @@ def loadFeaturesForBook(filename, smartStopWords, pronSet, conjSet):
 
     return result
 
-def withPipeline(x, y):
-    clfer = Pipeline([('scaler',StandardScaler()),('linear_model',SGDClassifier())])
-    cval = KFold(x.shape[0], NUMFOLDS, shuffle=True, random_state=42)
-    score = cross_val_score(clfer, x, y, cv=cval) # reports estimator accuracy
+def withCrossFoldValidation(x, y, scoring):
+    # feature selection since we have a small sample space
+    fs = SelectPercentile(scoring, percentile=20)
+
+    pipeline = Pipeline([('featureselector',fs),('scaler',StandardScaler()),('estimator',SGDClassifier())])
+
+    # StratifiedShuffleSplit returns stratified splits, i.e train/test splits that
+    # preserve the same percentage for each target class as in the complete set.
+    # Better than k-Fold shuffle since it allows finer control over samples on each
+    # side of the train/test split.
+    cval = StratifiedShuffleSplit(y, n_iter=NUMFOLDS, test_size=.30)
+
+    score = cross_val_score(pipeline, x, y, cv=cval) # reports estimator accuracy
     print "%2.3f (+/- %2.3f)" % (np.mean(score), sem(score))
 
-def withoutPipeline(x, y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42) # 25% reserved for validation
+def withoutCrossFoldValidation(x, y, scoring):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42) # 30% reserved for validation
 
-    scaler = StandardScaler().fit(x_train) # for each x, (x - mean(all x))/std. dev. of x
-                                           # this step computes the mean and std. dev.
-    x_train = scaler.transform(x_train)
-    x_test = scaler.transform(x_test)
-    clfer = SGDClassifier() # use Stochastic Gradient Descent
-    clfer.fit(x_train, y_train)
+    # feature selection since we have a small sample space
+    fs = SelectPercentile(scoring, percentile=20)
 
-    # validate results
+    pipeline = Pipeline([('featureselector',fs),('scaler',StandardScaler()),('estimator',SGDClassifier())])
+
+    clfer = pipeline.fit(x_train, y_train)
     y_predict_train = clfer.predict(x_train)
-    print "% Correct results on training set:"
-    print metrics.accuracy_score(y_train, y_predict_train)
+
+    print "%% Accuracy on training set: %2.3f" % metrics.accuracy_score(y_train, y_predict_train)
+
     y_predict_test = clfer.predict(x_test)
-    print "\n% Correct results on testing set:"
-    print metrics.accuracy_score(y_test, y_predict_test)
+    print "\n%% Accuracy on testing set: %2.3f" % metrics.accuracy_score(y_test, y_predict_test)
+
     print "\nClassification Report:"
     print metrics.classification_report(y_test, y_predict_test)
+
     print "Confusion Matrix:"
     print metrics.confusion_matrix(y_test, y_predict_test)
 
@@ -177,9 +205,9 @@ def createLegomenaPlot(x, y):
 # diagnostic plot
 def createSentenceDistributionPlot(x,y):
     barwidth = 0.3
-    tomsawyer = loadFeaturesForBook('text/0/pg74.txt')[3:28]
-    huckfinn = loadFeaturesForBook('text/0/pg76.txt')[3:28]
-    princepauper = loadFeaturesForBook('text/0/pg1837.txt')[3:28]
+    tomsawyer = loadFeaturesForBook('corpus/0/pg74.txt')[3:28]
+    huckfinn = loadFeaturesForBook('corpus/0/pg76.txt')[3:28]
+    princepauper = loadFeaturesForBook('corpus/0/pg1837.txt')[3:28]
     m = np.arange(len(tomsawyer))
     pt.bar(m,tomsawyer,barwidth,label='Tom Sawyer',color='r')
     pt.bar(m+barwidth,huckfinn,barwidth,label='Huck Finn',color='b')
@@ -204,7 +232,7 @@ if __name__ == '__main__':
     conjSet = buildConjSet()
     smartStopWords = buildStopWordsSet()
 
-    fileList = getFiles('text')
+    fileList = getFiles('corpus')
     x,y = loadBookData(fileList, smartStopWords, pronSet, conjSet)
-    withPipeline(x,y)
+    withCrossFoldValidation(x,y,f_classif)
 
