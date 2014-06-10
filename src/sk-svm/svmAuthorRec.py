51278cab61815d99ca783a7cd94bd1797a162170
Raj Rao <neeraj2608@gmail.com>
2014-06-10 14:36:14 -0400

    Added file to save features. Cuts down running time.

--- src/sk-svm/svmAuthorRec.py
+++ src/sk-svm/svmAuthorRec.py
@@ -17,7 +17,8 @@ import numpy as np
 import matplotlib.pyplot as pt
 
 NUMFOLDS = 5
-RANGE = 10
+RANGE = 16
+FEATURESFILE = 'bookfeatures.txt'
 
 class MyFreqDist(FreqDist):
     '''
@@ -117,19 +118,19 @@ def loadFeaturesForBook(filename, smartStopWords, pronSet, conjSet):
 
     #sentenceDist = FreqDist(sentences)
     #print sentenceDist.keys()[:15] # most common sentences
-    wordsDist = MyFreqDist(FreqDist(allWords))
-    #print wordsDist.keys()[:15] # most common words
+    wordsDist = MyFreqDist(FreqDist(cleanWords))
+    #print wordsDist.keys()[:30] # most common words
     #print wordsDist.keys()[-15:] # most UNcommon words
 
     numUniqueWords = len(wordsDist.keys())
-    numTotalWords = len(allWords)
+    numTotalWords = len(cleanWords)
 
     hapax = float(len(wordsDist.hapaxes()))/numUniqueWords # no. words occurring once / total num. UNIQUE words
     dis = float(len(wordsDist.dises()))/numUniqueWords # no. words occurring twice / total num. UNIQUE words
     richness = float(numUniqueWords)/numTotalWords # no. unique words / total num. words
 
     sentenceLengthDist = map(lambda x: sentenceLengthDistribution.freq(x), range(1,RANGE))
-    wordLengthDist = map(lambda x: wordLengthDistribution.freq(x), range(4,18))
+    wordLengthDist = map(lambda x: wordLengthDistribution.freq(x), range(1,RANGE))
     pronounDist = map(lambda x: pronounDistribution.freq(x), range(1,RANGE))
     conjunctionDist = map(lambda x: conjunctionDistribution.freq(x), range(1,RANGE))
 
@@ -163,11 +164,10 @@ def withCrossFoldValidation(x, y, scoring):
     # preserve the same percentage for each target class as in the complete set.
     # Better than k-Fold shuffle since it allows finer control over samples on each
     # side of the train/test split.
-    cval = StratifiedShuffleSplit(y, n_iter=NUMFOLDS, test_size=.30)
+    cval = StratifiedShuffleSplit(y, n_iter=NUMFOLDS, test_size=.25)
 
     score = cross_val_score(pipeline, x, y, cv=cval) # reports estimator accuracy
     print "%2.3f (+/- %2.3f)" % (np.mean(score), sem(score))
-    return pipeline
 
 def withoutCrossFoldValidation(x, y, scoring):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42) # 30% reserved for validation
@@ -221,7 +221,7 @@ def createSentenceDistributionPlot(x,y):
     pt.xticks(m)
     pt.show()
 
-def loadBookData(dirList, fileList, smartStopWords, pronSet, conjSet):
+def loadBookDataFromCorpus(dirList, fileList, smartStopWords, pronSet, conjSet):
     x = []
     y = []
     for index, files in enumerate(fileList):
@@ -229,14 +229,45 @@ def loadBookData(dirList, fileList, smartStopWords, pronSet, conjSet):
             y.append(dirList[index])
             x.append(loadFeaturesForBook(f, smartStopWords, pronSet, conjSet))
     le = LabelEncoder().fit(y)
-    return np.array(x), np.array(le.transform(y))
+    return np.array(x), np.array(le.transform(y)), le
 
-if __name__ == '__main__':
-    pronSet = buildPronounSet()
-    conjSet = buildConjSet()
-    smartStopWords = buildStopWordsSet()
+def loadBookDataFromFeaturesFile():
+    contents = open(FEATURESFILE,'rb').read().strip().split('\n')
+    x = []
+    y = []
+    for line in contents:
+        l = line.split('\t')
+        y.append(int(l[1]))
+        x.append(map(float,l[2].split(',')))
+    return np.array(x), np.array(y)
 
-    dirList, fileList = getFiles('corpus')
-    x,y = loadBookData(dirList, fileList, smartStopWords, pronSet, conjSet)
+def saveBookFeatures(x,y,le):
+    f = open(FEATURESFILE,'wb')
+    for index,item in enumerate(x):
+        f.write("%s\t%d\t%s\n" % (le.inverse_transform(y[index]),y[index],','.join(map(str,item))))
+    f.close()
+
+def runClassification():
+    x = []
+    y = []
+    if not path.exists(FEATURESFILE):
+        print '{0} not found. Creating...'.format(FEATURESFILE)
+        pronSet = buildPronounSet()
+        conjSet = buildConjSet()
+        smartStopWords = buildStopWordsSet()
+
+        dirList, fileList = getFiles('corpus')
+        #dirList = ['hermann-melville','mark-twain'] # testing
+        #fileList = [['corpus/herman-melville/pg2701.txt'],['corpus/mark-twain/pg74.txt']] # testing
+        x,y,le = loadBookDataFromCorpus(dirList, fileList, smartStopWords, pronSet, conjSet)
+        saveBookFeatures(x,y,le)
+        print '... done.'
+    else:
+        print '{0} found. Reading...'.format(FEATURESFILE)
+        x,y = loadBookDataFromFeaturesFile()
+
+    print 'Running classification'
     withCrossFoldValidation(x,y,f_classif) # use ANOVA scoring
 
+if __name__ == '__main__':
+    runClassification()
